c12f6399bb7dded30bf3dfb496c6059218fadf9d
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2022-11-15 23:57:05 -0600

    ejemplos de variables.

---
+++ 2.Variables/Variables.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//Sintaxis de declaracion de variables 
+	//tipoDato NombreVariable = InicializacionVariable;
+	
+	//Ejemplos de declaracion de variables
+	int Entero = 0;
+	double Decimal = 0;
+	float ComaFlotante = 0;
+	bool Binario = false;
+	char Caracter = 'a';
+	string Cadena = "";
+	
+	//Ejemplo de Asignacion de variables
+	Entero = 10;
+	Decimal = 10.2;
+	ComaFlotante =20.25;
+	Binario = true;
+	Caracter = 'u';
+	Cadena = "Software";
+		
+		
+	
+	
+	
+	
+	
+	return 0;
+}
+
