41abbd2e11508ed807d70c305f47b60d2451dd44
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2022-11-15 23:48:55 -0600

    variantes para imprimir en pantalla.

--- ImprimirEnPantalla/main.cpp
+++ ImprimirEnPantalla/main.cpp
@@ -5,7 +5,9 @@ int main(int argc, char *argv[]) {
 	
 	//Para el caso de C++ podemos utilizar cout y en el caso de C printf
 	
-	cout<<"Esto se imprime en pantalla"<<endl;
+	cout<<"Esto se imprime en pantalla en lenguaje c++"<<endl;
+	
+	printf("Esto se imprime en pantalla en lencuaje c");
 	
 	
 	return 0;
