d4d1366c42ab74455342f2bd7c7bb18702264397
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2022-11-15 23:45:19 -0600

    Ejemplo Imprimir en pantalla.

---
+++ ImprimirEnPantalla/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	//Para el caso de C++ podemos utilizar cout y en el caso de C printf
+	
+	cout<<"Esto se imprime en pantalla"<<endl;
+	
+	
+	return 0;
+}
+
